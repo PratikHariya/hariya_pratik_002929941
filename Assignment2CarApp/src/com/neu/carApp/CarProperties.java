58cff820adfc32a464521b12369835dc418cef09
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2022-02-18 01:17:55 -0500

    Create and Update

---
+++ Assignment2CarApp/src/com/neu/carApp/CarProperties.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neu.carApp;
+
+/**
+ *
+ * @author 18573
+ */
+public class CarProperties {
+    
+    private String carName;
+    private boolean passengerCar;
+    private boolean availability;
+    private String manufacturer;
+    private int yearOfManufacture;
+    private int seats;
+    private int serialNumber;
+    private int modelNumber;
+    private boolean validity;
+    private String city;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public boolean isPassengerCar() {
+        return passengerCar;
+    }
+
+    public void setPassengerCar(boolean passengerCar) {
+        this.passengerCar = passengerCar;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public boolean isValidity() {
+        return validity;
+    }
+
+    public void setValidity(boolean validity) {
+        this.validity = validity;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+}
---
+++ Assignment2CarApp/src/com/neu/carApp/CarPropertyObjects.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neu.carApp;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author 18573
+ */
+public class CarPropertyObjects {
+    
+    HashMap<Integer, CarProperties> carMap = new HashMap<Integer, CarProperties>();
+
+    public HashMap<Integer, CarProperties> getCarMap() {
+        return carMap;
+    }
+
+    public void setCarMap(HashMap<Integer, CarProperties> carMap) {
+        this.carMap = carMap;
+    }
+    
+    
+    
+}
